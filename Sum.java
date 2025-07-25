73af04224d3abb112f53db69068c7c831166c69f
“Sxpkait” <>
2025-07-25 15:16:27 +0700

    Import Num1&Num2

--- Sum.java
+++ Sum.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Sum {
     public static void main (String args[]) {
-        int num1 =4;
-        int num2 =6;
+        // int num1 =4;
+        // int num2 =6;
+ 
+        Scanner Scanner=new Scanner (System.in);
+
+        System.out.print("Enter num1: ");
+        int num1 = Scanner.nextInt();
+        
+        System.out.print("Enter num2: ");
+        int num2 = Scanner.nextInt();
+
+
         int sum = num1+num2;
-        System.out.println("Sum of" +num1 + "and" +num2 +"=" +sum);
+        System.out.println("Sum of " +num1 + " and " +num2 +" = " +sum);
     }
 }
